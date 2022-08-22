39c7393b8dcbde2ca28147965e2cf2a19663ffa1
SACHINILANGOVAN <sachinilangovan2001@gmail.com>
2022-08-22 15:09:29 +0530

    commit

---
+++ src/main/java/com/nseit/spring/BinarySearchImpl.java
@@ -0,0 +1,11 @@
+package com.nseit.spring;
+
+public class BinarySearchImpl {
+    public int binarySearch(int[] numbers, int numberToSearch) {
+            BubbleSortAlg bubbleSortAlg = new BubbleSortAlg();
+        QuickSort quicksort = new QuickSort();
+
+        int[] sortedNumber = quicksort.sort(numbers);
+        return 12;
+    }
+}
---
+++ src/main/java/com/nseit/spring/BubbleSortAlg.java
@@ -0,0 +1,4 @@
+package com.nseit.spring;
+
+public class BubbleSortAlg {
+}
